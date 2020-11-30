08560a57e78da6c93c9607fdd7c381abfe62ad0b
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2020-11-30 16:42:18 +0300

    Delete backup.sh

--- back_params/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-chmod 600 /home/vagrant/private_key
-chown root /home/vagrant/private_key
-sudo BORG_PASSPHRASE="derparol" \
-BORG_RSH="ssh -o 'StrictHostKeyChecking=no' -i /home/vagrant/private_key" \
-borg create -v --stats root@192.168.10.10:/var/backup::'{now:%Y-%m-%d-%H-%M}' /etc \
-2>&1 | logger &
-
