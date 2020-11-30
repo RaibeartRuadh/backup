e4b8455644f328bd6b28a1f1be3d5ff0360d4494
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2020-11-30 16:42:55 +0300

    Delete server.sh

--- serv_params/server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo mkdir /var/backup
-sudo mkfs.xfs /dev/sdb
-sudo mount /dev/sdb /var/backup
-sudo BORG_PASSPHRASE="derparol" borg init --encryption=repokey-blake2 /var/backup
-
-#borg init --encryption=repokey-blake2 root@192.168.10.11:myrepo
-
-
