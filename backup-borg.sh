649138cf72414cc0cdaa998c1b92720041ce6aad
RaibeartRuadh <robroyway@gmail.com>
2021-02-02 12:05:58 +0300

    fix descriptions for best view

--- backup-borg.sh
+++ backup-borg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# лок на случай повторного запуска
+# temporary lock - лок на случай повторного запуска
 LOCKFILE=/tmp/lockfile
 if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
     echo "Service already working!"
@@ -9,7 +9,7 @@ fi
 trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
 echo $$ > ${LOCKFILE}
 
-# параметры окружения
+# environment - параметры окружения
 BACKUP_HOST='192.168.10.11'
 BACKUP_USER='borg'
 BACKUP_REPO='/var/backup'
@@ -18,21 +18,21 @@ export BORG_PASSPHRASE='derparol'
 
 echo $BACKUP_REPO
 
-# создание резервных записей
+# backup create params - создание резервных записей
 borg create \
   --stats --list --debug --progress \
   ${BACKUP_USER}@${BACKUP_HOST}:${BACKUP_REPO}::"var-backup-{now:%Y-%m-%d_%H:%M:%S}" \
   /etc 2>> ${LOG}
 
 
-# удаление резервных записей
+# delete elder records - удаление резервных записей
 borg prune \
   -v --list \
   ${BACKUP_USER}@${BACKUP_HOST}:${BACKUP_REPO} \
   --keep-within 1m \
   --keep-monthly 3 
 
-# удаляем лок
+# remove lock - удаляем лок
 rm -f ${LOCKFILE}
 
 
